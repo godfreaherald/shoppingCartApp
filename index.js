729663cb6b36f0f0e74ceb712942718542f5e97a
godfreaherald <wambusiness2015@gmail.com>
2021-06-19 13:00:08 +0300

    added reading from env and linting using eslint

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
 import productsRoute from './routes/products';
+
 const PORT = 8090;
+const { HOST_PORT } = process.env;
 
 const app = express();
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/products', productsRoute);
-
 app.use((req, res, next, error) => {
-    error.status = 404;
+    // error.status = 404;
     // next(error);
 });
 
-app.listen(PORT, () => {
-    console.log('Server is running... on port ' + PORT);
+app.listen(HOST_PORT || PORT, () => {
+    console.log(`Server is running... on port ${HOST_PORT}` || PORT);
 });
